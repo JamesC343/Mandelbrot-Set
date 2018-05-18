090a2b16398aadcfea73faebc8c1ce837d641270
James Cameron <james__ow@hotmail.com>
2018-05-18 19:50:09 +1200

    Add project files.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,172 @@
+#include "MainWindow.h"
+#include "Engine.h"
+#include "String.h"
+#include "Colors.h"
+#include "Math.h"
+
+
+Engine::Engine( MainWindow& wnd ) :	wnd( wnd )
+{
+	gfx = new Graphics(wnd);
+
+	test = new Surface("images/background.bmp");
+	
+	pixelZoom = graphScale / windowSize.x;
+}
+
+Engine::~Engine()
+{
+}
+
+void Engine::Go()
+{
+	gfx->BeginFrame();
+	UpdateModel();
+	ComposeFrame();
+	gfx->EndFrame();
+}
+
+void Engine::UpdateModel()
+{
+	if( wnd.kbd.KeyIsPressed( VK_LEFT ) )
+	{
+		iterations -= 10;
+	}
+	if( wnd.kbd.KeyIsPressed( VK_RIGHT ) )
+	{
+		iterations += 10;
+	}
+	if( wnd.kbd.KeyIsPressed( 'A' ) )
+	{
+		cLock = true;
+		drawColor = Colors::Black;
+	}
+	if( wnd.kbd.KeyIsPressed( 'S' ) )
+	{
+		cLock = false;
+		drawColor = Colors::Red;
+		iterations = 100;
+	}
+	if( wnd.kbd.KeyIsPressed( 'Z' ) )
+	{
+		//zoom in
+		zoomCamera(1.1);
+	}
+	if( wnd.kbd.KeyIsPressed( 'X' ) )
+	{
+		//zoom out
+		zoomCamera(1/1.1);
+	}
+
+	if (!cLock)
+	{
+		cReal = wnd.mouse.GetPosX() * 0.01 - 2;
+		cImaginary = wnd.mouse.GetPosY() * 0.01 - 2;
+	}
+	else if( iterations < 1000)
+	{
+		iterations *= 2;
+	}
+}
+
+void Engine::ComposeFrame()
+{
+	gfx->DrawSpriteNonChroma(0, 0, *test);
+	
+	Vei2 point(0, 0);
+
+	for (int i = 0; i < windowSize.x; i++)
+	{
+		for (int j = 0; j < windowSize.y; j++)
+		{
+			point.x = i;
+			point.y = j;
+
+			int result = window(point);
+
+			if (result > -1)
+				putPixel(point, result);
+		}
+	}
+}
+
+void Engine::putPixel(Vei2 pixel, int colorOption)
+{
+	if (pixel.x < 0)
+		pixel.x = 0;
+	else if (pixel.x >= 400)
+		pixel.x = 399;
+
+	if (pixel.y < 1)
+		pixel.y = 1;
+	else if (pixel.y > 400)
+		pixel.y = 400;
+
+	//static constexpr Color White = MakeRGB( 255u,255u,255u );
+	//static constexpr Color Black = MakeRGB( 0u,0u,0u );
+
+	//int a = (float)255 / iterations * colorOption;
+	int a = colorOption;
+	
+	Color asasdfasdf = ( 255u,255u,255u );
+	//Color drawColor = ( a + 'u',255u,255u );
+
+	gfx->PutPixel(pixel.x, windowSize.y - pixel.y, Color(colorOption));
+}
+
+int Engine::window(Vei2 screenCoordinate)
+{
+	double real = graphXMin + (screenCoordinate.x * pixelZoom);
+	double imaginary = graphYMin + (screenCoordinate.y * pixelZoom);
+	
+	return mandelbrotFunction(real, imaginary);
+	return juliaFunction(real, imaginary);
+}
+
+int Engine::mandelbrotFunction(double zX, double zY, double cX, double cY, int n)
+{
+	double cXIterating = cX * cX - cY * cY + zX;
+	double cYIterating = cX * cY * 2 + zY;
+	
+	if (cXIterating * cXIterating + cYIterating * cYIterating > 4)
+		return 16581375 - (10000000 / (sqrt(n)+1));
+
+	if (n > iterations)
+		return 0;
+
+	return mandelbrotFunction(zX, zY, cXIterating, cYIterating, n + 1);
+}
+
+int Engine::juliaFunction(double zX, double zY, int n)
+{
+	double zXIterating = zX * zX - zY * zY + cReal;
+	double zYIterating = zX * zY * 2 + cImaginary;
+	
+	if (zXIterating * zXIterating + zYIterating * zYIterating > 4)
+		return 16581375 - (13000000 / (sqrt(n)+1)) + (3000 / (zX * zY));
+
+	if (n > iterations)
+		return 0;
+
+	return juliaFunction(zXIterating, zYIterating, n + 1);
+}
+
+void Engine::zoomCamera(float zoom)
+{
+	iterations = 50;
+
+	double lockXPercent = (double)wnd.mouse.GetPosX() / windowSize.x;
+	double lockYPercent = 1 - (double)wnd.mouse.GetPosY() / windowSize.y;
+
+	double lockX = lockXPercent * graphScale + graphXMin;
+	double lockY = lockYPercent * graphScale + graphYMin;
+
+	cameraZoom *= zoom;
+	
+	graphScale = 4 / cameraZoom;
+	
+	graphXMin = lockX - graphScale * lockXPercent;
+	graphYMin = lockY - graphScale * lockYPercent;
+	
+	pixelZoom = graphScale / windowSize.x;
+}
---
+++ Engine/Engine.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "Graphics.h"
+#include "Colors.h"
+
+class Engine
+{
+public:
+	Engine( class MainWindow& wnd );
+	~Engine();
+	Engine( const Engine& ) = delete;
+	Engine& operator=( const Engine& ) = delete;
+	void Go();
+
+private:
+	void ComposeFrame();
+	void UpdateModel();
+	void putPixel(Vei2,int);
+	int window(Vei2);
+	int mandelbrotFunction(double,double,double=0,double=0,int=0);
+	int juliaFunction(double,double,int=0);
+	void zoomCamera(float);
+
+private:
+	MainWindow& wnd;
+	Graphics* gfx;
+	Surface* test;
+	
+	const Vei2 windowSize = { 400, 400 };
+	const Vei2 graphZeroOffset = { -200, -201 };
+	
+	double graphXMin = -2;
+	double graphYMin = -2;
+	double graphScale = 4;
+
+	double pixelZoom = 0;
+	double cameraZoom = 1;
+
+	double cReal = 0;
+	double cImaginary = 0;
+
+	bool cLock = false;
+	Color drawColor = Colors::Red;
+
+	int iterations = 100;
+
+};
+
